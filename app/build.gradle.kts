d12dd191fa47f802bca70d8e529e62a8e2a47c86
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2024-12-01 17:01:46 +1100

    init

---
+++ app/build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation(libs.junit.jupiter.engine)
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+application {
+    mainClass = "dev.lennartegb.AppKt"
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
---
+++ app/src/main/kotlin/dev/lennartegb/Day1.kt
@@ -0,0 +1,5 @@
+package dev.lennartegb
+
+fun main() {
+
+}
