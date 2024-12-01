d6958a60a6e6915832ab0bc2310582bcf7ac4f8d
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2024-12-01 21:16:11 +1100

    prepare for day2

---
+++ day2/src/test/kotlin/MainTest.kt
@@ -0,0 +1,14 @@
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class MainTest {
+    @Test
+    fun part1() {
+
+    }
+
+    @Test
+    fun part2() {
+
+    }
+}
--- utils/build.gradle.kts
+++ utils/build.gradle.kts
@@ -3,17 +3,18 @@ plugins {
     id("java-library")
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
-}
-
 dependencies {
     api(libs.kotlin.io)
 }
 
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
 kotlin {
     compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
     }
 }
