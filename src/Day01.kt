45688da12b40765da5446c56f12f5a2238979a6a
LennartEgb <26793300+LennartEgb@users.noreply.github.com>
2024-12-03 15:29:53 +1100

    Day 1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,37 @@
+import kotlin.math.absoluteValue
+import kotlin.time.measureTimedValue
+
+fun List<String>.toLRLists(): Pair<List<Int>, List<Int>> {
+    val left = mutableListOf<Int>()
+    val right = mutableListOf<Int>()
+    forEach { row ->
+        if (row.isBlank()) return@forEach
+        val (l, r) = row.split("   ")
+        left.add(l.toInt())
+        right.add(r.toInt())
+    }
+    return left to right
+}
+
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(content: List<String>): Int {
+        val (left, right) = content.toLRLists()
+        val rightSorted = right.sorted()
+        val leftSorted = left.sorted()
+        return leftSorted.mapIndexed { index, l ->
+            val r = rightSorted[index]
+            (r - l).absoluteValue
+        }.sum()
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(content: List<String>): Int {
+        val (left, right) = content.toLRLists()
+        val rightMap = right.groupBy { it }.mapValues { it.value.size }
+        return left.sumOf { it * rightMap.getOrDefault(it, 0) }
     }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
-
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
+    val input = readInput(name = "Day01")
     part1(input).println()
     part2(input).println()
 }
